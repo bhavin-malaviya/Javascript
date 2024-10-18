268c82a83df81dece1ecd2663bcde4dd1cbc5f44
bhavin malaviya <bhavinmalaviya88@gmail.com>
2024-10-18 12:40:20 +0530

    newJavaScript

---
+++ String.js
@@ -0,0 +1,40 @@
+const gameName = 'bhavin-malaviya';
+
+const matchword = gameName.match('bhav');
+console.log(matchword);
+
+const lengthWord = gameName.length;
+console.log(lengthWord);
+
+const indexOfWord = gameName.indexOf('a')
+console.log(indexOfWord);
+
+// charAt
+// replace
+// repeat
+// search
+// split
+// slice
+// substring
+// sup
+// toLowerCase
+// toUpperCase
+// trim
+// toFixed
+// toPrecision
+// toLocaleString
+
+//    # Math
+
+// abs
+// round
+// ceil
+// floor
+// sqrt
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
