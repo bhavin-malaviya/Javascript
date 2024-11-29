1e476864c84daf21bd2bc67eda961b546ea3a582
bhavin malaviya <bhavinmalaviya88@gmail.com>
2024-11-29 16:54:58 +0530

    scope

--- 06_Scope.js
+++ 06_Scope.js
@@ -6,8 +6,57 @@ if(true){
     var c = 30;
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 
+
+//  Global Socpe Are differences between node and windows.
+
+
+function one() {
+    const userName = "Bhavin"
+
+    function two(){
+        const wbsites = "youtube"
+        console.log(userName);
+    }
+    // console.log(wbsites);      /* this one not accessible because its out scope*/
+
+    two();
+    
+}
+
+// one();
+ 
+
+if(true){
+    const userName = "Bhavin";
+    if(true){
+        const websites = " youtube";
+        console.log(userName + websites);
+    }
+
+    // console.log(wbsites);  /* this one not accessible because its out of if statement*/
+    
+}
+
+// console.log(userName);  /* this one not accessible because its out of if statement*/
+
+
+
+// ----------------------------------------------------------------------------------------------------------
+
+
+console.log(addone(5));
+function addone(num){
+    return num + 1;
+}
+
+//  if function store in variables that you can not access in front of the function
+
+console.log(addTwo(5));
+const addTwo = function(num) {
+    return num + 2;
+}
 
