b48b9942943876c1d6979cf12b5e07f94936ab0d
bhavin malaviya <bhavinmalaviya88@gmail.com>
2025-01-03 14:55:41 +0530

    img

---
+++ Events_JavaScript/image.js
@@ -0,0 +1,8 @@
+document.getElementById("21blitz").addEventListener("click", (e) => {
+    e.stopPropagation();
+    console.log("21Blitz clicked");
+}, false);
+
+document.getElementById("images").addEventListener("click", (e) => {
+    console.log("Click Inside the ul");
+}, false);
